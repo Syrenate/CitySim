c1a817b9d52d0857766156c3e9ffcfd7ce2c759c
Syrenate <lyric.jennings@outlook.com>
2026-09-11 20:08:38 +0100

    Cleaner camera controls

---
+++ build/src/roads/CMakeFiles/road_lib.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/road_lib.dir/JunctionDesign.cpp.o"
+  "CMakeFiles/road_lib.dir/JunctionDesign.cpp.o.d"
+  "CMakeFiles/road_lib.dir/RoadDesign.cpp.o"
+  "CMakeFiles/road_lib.dir/RoadDesign.cpp.o.d"
+  "CMakeFiles/road_lib.dir/RoadNetwork.cpp.o"
+  "CMakeFiles/road_lib.dir/RoadNetwork.cpp.o.d"
+  "CMakeFiles/road_lib.dir/RoadRendering.cpp.o"
+  "CMakeFiles/road_lib.dir/RoadRendering.cpp.o.d"
+  "CMakeFiles/road_lib.dir/VectorMath.cpp.o"
+  "CMakeFiles/road_lib.dir/VectorMath.cpp.o.d"
+  "libroad_lib.a"
+  "libroad_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/road_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
